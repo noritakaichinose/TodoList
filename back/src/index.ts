325bf804306708b8b81672e7d93f11ace560fd12
noritaka.ichinose <happppppiness@icloud.com>
2024-08-14 07:40:40 +0900

    tets

--- back/src/index.ts
+++ back/src/index.ts
@@ -11,9 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
+app.use(cors());
 
 app.use(express.json());
 
