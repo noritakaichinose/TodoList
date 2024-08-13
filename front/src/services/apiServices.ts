87ccfc555add3ae14ecc37d7956c3c38be7a5d28
noritaka.ichinose <happppppiness@icloud.com>
2024-08-14 07:29:22 +0900

    remove env

--- front/src/services/apiServices.ts
+++ front/src/services/apiServices.ts
@@ -2,12 +2,11 @@ import axios from 'axios';
 import { getToken } from './authService';
 import { Todo } from '../types/todo';
 
-const API_URL = process.env.REACT_APP_API_URL !== null ? process.env.REACT_APP_API_URL : "";
 
 export const fetchTodos = async () => {
   const token = getToken();
   try {
-    const response = await axios.get(`${API_URL}/api/`, {
+    const response = await axios.get('/api/', {
       headers: { Authorization: `Bearer ${token}` }
     });
     return response;
@@ -20,7 +19,7 @@ export const fetchTodos = async () => {
 export const addTodo = async (title: string) => {
   const token = getToken();
   try {
-    const response = await axios.post(`${API_URL}/api/add`, { title }, {
+    const response = await axios.post('/api/add', { title }, {
       headers: { Authorization: `Bearer ${token}` }
     });
     return response;
@@ -33,7 +32,7 @@ export const addTodo = async (title: string) => {
 export const deleteTodo = async (id: string) => {
   const token = getToken();
   try {
-    await axios.delete(`${API_URL}/api/delete/${id}`, {
+    await axios.delete(`/api/delete/${id}`, {
       headers: { Authorization: `Bearer ${token}` }
     });
   } catch (err: any) {
@@ -45,7 +44,7 @@ export const deleteTodo = async (id: string) => {
 export const editTodo = async (id: string, updatedTodo: Todo) => {
   const token = getToken();
   try {
-    const response = await axios.put(`${API_URL}/api/update/${id}`, updatedTodo, {
+    const response = await axios.put(`/api/update/${id}`, updatedTodo, {
       headers: { Authorization: `Bearer ${token}` }
     });
     return response;
